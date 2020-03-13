f1103ec95500b9e13407b7a8bcffe7eb4c6493bf
Nicholas Tidwell <nicktidwell52@gmail.com>
2020-03-13 10:11:05 -0400

    Update and refactor iris.py PCA, prep mapper file

--- iris.py
+++ iris.py
@@ -1,33 +1,20 @@
-
 import matplotlib.pyplot as plt
 import matplotlib.colors
 from sklearn import datasets
-import sklearn
 from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
-
 
 digits = datasets.load_digits()
 
-scaler=StandardScaler()
-X_scaled = scaler.fit_transform(digits.data)
-
 pca=PCA(n_components=2)
-X_pca=pca.fit_transform(X_scaled)
+X_pca=pca.fit_transform(digits.data)
 
 kmeans = KMeans(n_clusters=10)
 kmeans.fit(X_pca)
 labels = kmeans.predict(X_pca)
-centroids = kmeans.cluster_centers_
-colors = ['red', 'blue', 'yellow', 'black', 'green', 'cyan', 'magenta', 'maroon', 'navy', 'orange']
-cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", colors)
-Xax=X_pca[:,0]
-Yax=X_pca[:,1]
 
-plt.scatter(Xax, Yax, c=labels, s=50, cmap=cmap)
-#plt.scatter(centroids[:, 0], centroids[:, 1], marker='*', c='#050505', s=1000)
-
+cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ['red', 'blue', 'yellow', 'black', 'green', 'cyan', 'magenta', 'maroon', 'navy', 'orange'])
+plt.scatter(X_pca[:,0], X_pca[:,1], c=labels, s=50, cmap=cmap)
 plt.show()
 
 
